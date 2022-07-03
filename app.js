5ebc9bef975319f87d6413989074668a311490c5
Nicholas Emmanuel Chimaobi <nicholasemmanuel321@gmail.com>
2022-07-03 20:21:15 +0100

    fixed remove class

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ class Book {
     this.author = author;
   }
 }
-
 // =========== Store class: Handles local storage ===========
 class Store {
   static getBooks() {
@@ -24,18 +23,16 @@ class Store {
     localStorage.setItem('books', JSON.stringify(books));
   }
 
-  static removeBook(del) {
+  static removeBook(i) {
     const books = Store.getBooks();
     books.forEach((book, index) => {
-      if (book.del === del) {
+      if (index === i) {
         books.splice(index, 1);
       }
     });
-
     localStorage.setItem('books', JSON.stringify(books));
   }
 }
-
 // ============ UI class: Handle ui task ============
 class UI {
   static displayBooks() {
@@ -45,19 +42,18 @@ class UI {
 
   static addBookToList(book) {
     const list = document.querySelector('#book-list');
-
     const row = document.createElement('tr');
     row.innerHTML = `
-  <td>${book.title}</td>
-  <td>${book.author}</td>
-  <td><a href="#" class="delete">Remove</td>
+  <td class="title">${book.title}</td>
+  <td class="author">${book.author}</td>
+  <td class="delete">Remove</td>
   `;
     list.appendChild(row);
   }
 
   static deleteBook(del) {
     if (del.classList.contains('delete')) {
-      del.parentElement.parentElement.remove();
+      del.parentElement.remove();
     }
   }
 
@@ -66,38 +62,25 @@ class UI {
     document.querySelector('#author').value = '';
   }
 }
-
 // ============ Event: Display Books ============
-
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
-
 // =========== Event: Add a Book ===========
-
 document.querySelector('#book-form').addEventListener('submit', (e) => {
   e.preventDefault();
-
   // get the form value
-
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
-
   if (!title || !author) return;
   const book = new Book(title, author);
   UI.addBookToList(book);
-
   Store.addBook(book);
-
   UI.clearFields();
 });
-
 // =========== Event: Remove a Book =============
-
 document.querySelector('#book-list').addEventListener('click', (e) => {
   // remove book from UI
-
+  const btns = Array.from(document.querySelectorAll('.delete'));
   UI.deleteBook(e.target);
-
   // remove book from local store
-
-  Store.removeBook();
+  Store.removeBook(btns.indexOf(e.target));
 });
